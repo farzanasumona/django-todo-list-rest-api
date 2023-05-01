1ed3a41f4ffd6b16666b42ec1dbce85aca318bae
farzanasumona <farzanasumona@gmail.com>
2023-05-01 05:06:01 +0200

    updated docker-compose.yml and settings.py

--- TodoListApi/settings.py
+++ TodoListApi/settings.py
@@ -79,11 +79,17 @@ WSGI_APPLICATION = 'TodoListApi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
         'HOST': 'db',
         'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+            'sql_mode': 'traditional',
+            'sslmode': 'require',
+            'path': '/path/to/psql',
+        }
     }
 }
 
